895a9b3dad0a19748f5e140c64ba9f7c23c088f4
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-03-24 18:55:51 +0200

    Improve the ConsoleManger class

--- src/main/java/com/anas/jcolorfulconsole/ConsoleManger.java
+++ src/main/java/com/anas/jcolorfulconsole/ConsoleManger.java
@@ -1,39 +1,42 @@
 package com.anas.jcolorfulconsole;
 
+import com.anas.jcolorfulconsole.color.ASCIIColor;
+
 public interface ConsoleManger {
-    void setColor(ASCIIColor color);
+    void setColor(ConsoleProperty property);
     void setBackgroundColor(ASCIIColor color);
+    void setProperty(ConsoleProperty property);
 
     void print(String text);
-    void print(String text, ASCIIColor color);
+    void print(String text, ConsoleProperty property);
     void println(String text);
-    void println(String text, ASCIIColor color);
+    void println(String text, ConsoleProperty property);
     void println();
 
     void print(char c);
-    void print(char c, ASCIIColor color);
+    void print(char c, ConsoleProperty property);
     void println(char c);
-    void println(char c, ASCIIColor color);
+    void println(char c, ConsoleProperty property);
 
     void print(int i);
-    void print(int i, ASCIIColor color);
+    void print(int i, ConsoleProperty property);
     void println(int i);
-    void println(int i, ASCIIColor color);
+    void println(int i, ConsoleProperty property);
 
     void print(double d);
-    void print(double d, ASCIIColor color);
+    void print(double d, ConsoleProperty property);
     void println(double d);
-    void println(double d, ASCIIColor color);
+    void println(double d, ConsoleProperty property);
 
     void print(boolean b);
-    void print(boolean b, ASCIIColor color);
+    void print(boolean b, ConsoleProperty property);
     void println(boolean b);
-    void println(boolean b, ASCIIColor color);
+    void println(boolean b, ConsoleProperty property);
 
     void print(Object o);
-    void print(Object o, ASCIIColor color);
+    void print(Object o, ConsoleProperty property);
     void println(Object o);
-    void println(Object o, ASCIIColor color);
+    void println(Object o, ConsoleProperty property);
 
     void printf(String format, Object... args);
 }
